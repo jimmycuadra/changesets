27ba4ece44f9e5eb386a62d940b9c984c007f653
Jimmy Cuadra <github@jimmycuadra.com>
2010-04-02 17:50:45 -0700

    adds shoulda tests for Changeset

--- test/unit/changeset_test.rb
+++ test/unit/changeset_test.rb
@@ -1,4 +1,19 @@
 require 'test_helper'
 
 class ChangesetTest < ActiveSupport::TestCase
+  context "A changeset" do
+    setup do
+      @changeset = Factory(:changeset)
+    end
+
+    subject { @changeset }
+
+    should_validate_numericality_of :revision, :ticket
+    should_not_allow_values_for :revision, 0, 1.1
+    should_not_allow_values_for :ticket, 0, 1.1
+    should_validate_presence_of :revision
+    should_validate_uniqueness_of :revision
+    should_allow_values_for :merged, true, false
+    should_have_named_scope :unmerged, :conditions => { :merged => false }
+  end
 end
