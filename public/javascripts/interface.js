3e44117e62bdb0dc300b6a45256311ad56b1d435
Jimmy Cuadra <jimmy@eventful.com>
2010-11-22 14:44:59 -0800

    add handlers for new/save/cancel buttons, update awesome buttons styles to work for disabled buttons, change colors of merge button

--- public/javascripts/interface.js
+++ public/javascripts/interface.js
@@ -1,5 +1,31 @@
 CHANGESETS.Interface = function () { };
 
 CHANGESETS.Interface.prototype.init = function () {
-  console.log('Interface initialized.');
+  this.jqEditFrame = $('#current-wrapper');
+  this.jqBtnNew = $('#btn-new');
+  this.jqBtnSave = $('#btn-save');
+  this.jqBtnCancel = $('#btn-cancel');
+
+  // button handlers
+  this.jqBtnNew.click(this.listenClickNew.bind(this), false);
+  this.jqBtnSave.click(this.listenClickSave.bind(this), false);
+  this.jqBtnCancel.click(this.listenClickSave.bind(this), false);
+};
+
+
+// button handlers
+
+CHANGESETS.Interface.prototype.listenClickNew = function () {
+  this.jqEditFrame.removeClass('hidden');
+  this.jqBtnNew.attr('disabled', 'disabled');
+};
+
+CHANGESETS.Interface.prototype.listenClickSave = function () {
+  this.jqEditFrame.addClass('hidden');
+  this.jqBtnNew.attr('disabled', '');
+};
+
+CHANGESETS.Interface.prototype.listenClickCancel = function () {
+  this.jqEditFrame.addClass('hidden');
+  this.jqBtnNew.attr('disabled', '');
 };
