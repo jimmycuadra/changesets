c232dce39920bdcafb3ea1d6869b7a0817f4666d
Jimmy Cuadra <jimmy@eventful.com>
2010-11-23 17:00:12 -0800

    add pagination styles and workaround for per_page bug in 3.0.pre2

--- app/controllers/changesets_controller.rb
+++ app/controllers/changesets_controller.rb
@@ -1,10 +1,12 @@
 class ChangesetsController < ApplicationController
+  # class-based per_page is broken in will_paginate 3.0.pre2
+
   def index
-    @changesets = Changeset.unmerged.paginate(:page => params[:page], :order => 'revision DESC')
+    @changesets = Changeset.unmerged.paginate(:page => params[:page], :order => 'revision DESC', :per_page => 10)
   end
 
   def all
-    @changesets = Changeset.paginate(:page => params[:page], :order => 'revision DESC')
+    @changesets = Changeset.paginate(:page => params[:page], :order => 'revision DESC', :per_page => 10)
     render :index
   end
 
