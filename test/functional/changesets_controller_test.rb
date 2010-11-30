7fcda9fc3a492981e8bad4fbceb9ebfe58bcf296
Jimmy Cuadra <jimmy@jimmycuadra.com>
2010-11-29 17:36:34 -0800

    add functional tests for index, all, and create

--- test/functional/changesets_controller_test.rb
+++ test/functional/changesets_controller_test.rb
@@ -1,4 +1,41 @@
 require 'test_helper'
 
 class ChangesetsControllerTest < ActionController::TestCase
+  context "on GET to index" do
+    setup do
+      get :index
+    end
+
+    should assign_to(:changesets)
+    should respond_with(:success)
+    should render_template(:index)
+  end
+
+  context "on GET to all" do
+    setup do
+      get :all
+    end
+
+    should assign_to(:changesets)
+    should respond_with(:success)
+    should render_template(:index)
+  end
+
+  context "on valid POST to create" do
+    setup do
+      post :create, :format => 'json', :changeset => Factory.attributes_for(:changeset)
+    end
+
+    should respond_with(:success)
+    should render_template(:create)
+  end
+
+  context "on invalid POST to create" do
+    setup do
+      post :create, :format => 'json'
+    end
+
+    should respond_with(:success)
+    should render_template(:errors)
+  end
 end
