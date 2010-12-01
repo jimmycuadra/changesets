dc3c85c323a3d7efca1d9cde4c67d25ab036e138
Jimmy Cuadra <jimmy@eventful.com>
2010-11-30 17:23:02 -0800

    use environment variables instead of app config yaml file

--- app/helpers/changesets_helper.rb
+++ app/helpers/changesets_helper.rb
@@ -1,9 +1,13 @@
 module ChangesetsHelper
   def changeset_link(changeset)
-    APP_CONFIG['changeset_url'].sub(/__CHANGESET__/, changeset)
+    return '' if ENV['CHANGESET_URL'].nil? || ENV['ISSUE_TRACKER'].nil?
+
+    "<a href=\"#{ENV['CHANGESET_URL'].sub(/__VALUE__/, changeset.revision.to_s)}\" target=\"_blank\">[#{ENV['ISSUE_TRACKER']}]</a>".html_safe
   end
   
-  def ticket_link(ticket)
-    APP_CONFIG['ticket_url'].sub(/__TICKET__/, ticket)
+  def ticket_link(changeset)
+    return '' if ENV['TICKET_URL'].nil? || ENV['ISSUE_TRACKER'].nil?
+
+    "<a href=\"#{ENV['TICKET_URL'].sub(/__VALUE__/, changeset.ticket.to_s)}\" target=\"_blank\">[#{ENV['ISSUE_TRACKER']}]</a>".html_safe
   end
 end
