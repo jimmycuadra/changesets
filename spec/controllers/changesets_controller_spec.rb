8d32ea1ae848926affc212b8826e3683d8c4e434
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-02-10 14:01:28 -0800

    add specs for changesets#update

--- spec/controllers/changesets_controller_spec.rb
+++ spec/controllers/changesets_controller_spec.rb
@@ -48,4 +48,38 @@ describe ChangesetsController do
       end
     end
   end
+
+  describe "#update" do
+    before(:each) do
+      @changeset = Factory(:changeset)
+    end
+
+    context "when valid" do
+      before(:each) do
+        put :update, :id => @changeset.to_param, :changeset => { :description => "updated" }, :format => :json
+      end
+
+      it "saves the record" do
+        assigns(:changeset).errors.should be_empty
+      end
+
+      it "renders the create template" do
+        response.should render_template("changesets/create")
+      end
+    end
+
+    context "when invalid" do
+      before(:each) do
+        put :update, :id => @changeset.to_param, :changeset => { :revision => "foo" }, :format => :json
+      end
+
+      it "does not save the record" do
+        assigns(:changeset).errors.should_not be_empty
+      end
+
+      it "renders the error template" do
+        response.should render_template("changesets/errors")
+      end
+    end
+  end
 end
