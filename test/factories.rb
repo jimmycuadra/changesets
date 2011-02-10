d59674bdbb0b2578c6eb302409afc5e0dc881d7e
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-02-10 14:05:43 -0800

    remove test directory

--- test/factories.rb
+++
@@ -1,6 +0,0 @@
-Factory.define :changeset do |c|
-  c.revision 123456
-  c.ticket 1234
-  c.description 'Updated some files'
-  c.notes 'OK to merge'
-end
--- test/unit/helpers/changesets_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ChangesetsHelperTest < ActionView::TestCase
-end
