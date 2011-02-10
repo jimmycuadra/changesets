45c3626160dc8caf4d4e34d31444483ff295dead
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-02-10 08:53:46 -0800

    set up spec_helper for simplecov and factory girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,24 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+# Simplecov setup
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/config/boot.rb"
+  add_group "Models", "/app/models"
+  add_group "Controllers", "/app/controllers"
+  add_group "Helpers", "/app/helpers"
+  add_group "Configuration", "/config"
+end
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/factories/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
@@ -18,7 +31,7 @@ RSpec.configure do |config|
   config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
