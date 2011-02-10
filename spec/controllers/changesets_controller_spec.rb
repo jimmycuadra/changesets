7ab4bd352ad7be95ff0da7a61c2f292994ce8c2c
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-02-10 13:51:26 -0800

    simplify specs for changesets#index, add specs for #all and #create

--- spec/controllers/changesets_controller_spec.rb
+++ spec/controllers/changesets_controller_spec.rb
@@ -1,18 +1,51 @@
 require 'spec_helper'
 
 describe ChangesetsController do
-  describe "#index" do
-    it "displays the last 10 unmerged changesets" do
-      changeset_ids = []
-      1.upto(11) do |n|
-        changeset = Factory(:changeset, :revision => n)
-        changeset_ids.push changeset.id
-      end
-      changeset_ids.reverse!
-      changeset_ids.pop
+  render_views
 
-      get :index
-      assigns(:changesets).map(&:id).should == changeset_ids
+  [:index, :all].each do |action|
+    describe "##{action}" do
+      before(:each) do
+        get action
+      end
+
+      it "should assign to changesets" do
+        assigns(:changesets).should be
+      end
+
+      it "should render the index template" do
+        response.should render_template(:index)
+      end
+    end
+  end
+
+  describe "#create" do
+    context "when valid" do
+      before(:each) do
+        post :create, :changeset => Factory.attributes_for(:changeset), :format => :json
+      end
+
+      it "saves the record" do
+        assigns(:changeset).should_not be_a_new_record
+      end
+
+      it "renders the create template" do
+        response.should render_template(:create)
+      end
+    end
+
+    context "when invalid" do
+      before(:each) do
+        post :create, :changeset => Factory.attributes_for(:changeset, :revision => nil), :format => :json
+      end
+
+      it "does not save the record" do
+        assigns(:changeset).should be_a_new_record
+      end
+
+      it "renders the error template" do
+        response.should render_template("changesets/errors")
+      end
     end
   end
 end
